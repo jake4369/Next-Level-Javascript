ebfd723d2d11fac7635467bde30f202088ba3961
Jake Samuels <samuels242@googlemail.com>
2022-07-07 19:14:39 +0100

    Render dice with random numbers

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -3,7 +3,6 @@ const hero = {
   name: "Wizard",
   avatar: "images/wizard.png",
   health: "60",
-  diceRoll: [3, 1, 4],
   diceCount: 3,
 };
 
@@ -12,23 +11,32 @@ const monster = {
   name: "Orc",
   avatar: "images/orc.png",
   health: "10",
-  diceRoll: [2],
   diceCount: 1,
 };
 
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+function getDiceRollArray(diceCount) {
+  let newDiceRolls = [];
+  for (let i = 0; i < diceCount; i++) {
+    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+  }
+  return newDiceRolls;
+}
 
-  const diceHtml = diceRoll
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
     .map((num) => `<div class="dice">${num}</div>`)
-    .join(" ");
+    .join("");
+}
+
+function renderCharacter(data) {
+  const { elementId, name, avatar, health, diceCount } = data;
 
   document.getElementById(`${elementId}`).innerHTML = `
     <div class="character-card">
         <h4 class="name">${name}</h4>
         <img class="avatar" src=${avatar} />
         <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">${diceHtml}</div>
+        <div class="dice-container">${getDiceHtml(diceCount)}</div>
     </div>
 `;
 }
