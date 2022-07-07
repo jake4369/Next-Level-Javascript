36f3cbbaf7aa22bda69a18ee78f1df3d62c2db0e
Jake Samuels <samuels242@googlemail.com>
2022-07-07 16:05:58 +0100

    Swap out for loop for map

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -3,7 +3,7 @@ const hero = {
   name: "Wizard",
   avatar: "images/wizard.png",
   health: "60",
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
   diceCount: 3,
 };
 
@@ -12,18 +12,16 @@ const monster = {
   name: "Orc",
   avatar: "images/orc.png",
   health: "10",
-  diceRoll: 4,
+  diceRoll: [2],
   diceCount: 1,
 };
 
 function renderCharacter(data) {
   const { elementId, name, avatar, health, diceRoll, diceCount } = data;
 
-  let diceHtml = "";
-
-  for (let i = 0; i < diceCount; i++) {
-    diceHtml += `<div class="dice">6</div>`;
-  }
+  const diceHtml = diceRoll
+    .map((num) => `<div class="dice">${num}</div>`)
+    .join(" ");
 
   document.getElementById(`${elementId}`).innerHTML = `
     <div class="character-card">
