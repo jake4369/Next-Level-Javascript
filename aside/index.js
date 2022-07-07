cb5b2b2d2c5df96b43705579accae1d4241faf14
Jake Samuels <samuels242@googlemail.com>
2022-07-07 15:19:03 +0100

    Aside: Object destructuring challenge

--- aside/index.js
+++ aside/index.js
@@ -12,3 +12,16 @@ const { title, year, genre, star, director } = favouriteFilm;
 console.log(
   `My favourite film is ${title} starring ${star}. It's an ${genre} film that was directed by ${director} and released in ${year}`
 );
+
+// Aside: Object destructuring challenge
+const dreamHoliday = {
+  destination: "Montego Bay, Jamaica",
+  activity: "swim in the sea",
+  accommodation: "Hotel Montego Bay",
+  companion: "Megan Fox",
+};
+
+const { destination, activity, accommodation, companion } = dreamHoliday;
+
+console.log(`I would love to go to ${destination}, to ${activity}. 
+I'd sleep at ${accommodation} and hang out with ${companion} all day.`);
