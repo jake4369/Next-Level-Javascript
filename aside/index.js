cdd8890ff1513ed1fdfc46a5ec9a3995e08aa7fa
Jake Samuels <samuels242@googlemail.com>
2022-07-07 15:49:30 +0100

    Aside: .map()

--- aside/index.js
+++ aside/index.js
@@ -1,4 +1,5 @@
 // Aside: Object destructuring
+/*
 const favouriteFilm = {
   title: "Top Gun",
   year: "1986",
@@ -12,8 +13,10 @@ const { title, year, genre, star, director } = favouriteFilm;
 console.log(
   `My favourite film is ${title} starring ${star}. It's an ${genre} film that was directed by ${director} and released in ${year}`
 );
+*/
 
 // Aside: Object destructuring challenge
+/*
 const dreamHoliday = {
   destination: "Montego Bay, Jamaica",
   activity: "swim in the sea",
@@ -25,3 +28,18 @@ const { destination, activity, accommodation, companion } = dreamHoliday;
 
 console.log(`I would love to go to ${destination}, to ${activity}. 
 I'd sleep at ${accommodation} and hang out with ${companion} all day.`);
+*/
+
+// Aside: .map()
+const energyCostEuros = [140, 153, 164, 153, 128, 146];
+const exchangeRate = 1.13;
+
+// const energyCostDollars = [];
+
+// for (let i = 0; i < energyCostEuros.length; i++) {
+//   energyCostDollars.push(energyCostEuros[i] * exchangeRate);
+// }
+
+const energyCostDollars = energyCostEuros.map((el) => el * exchangeRate);
+
+console.log(energyCostDollars);
