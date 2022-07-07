813e4646b3b4a5ab34f9026e8edaaba5f3515e80
Jake Samuels <samuels242@googlemail.com>
2022-07-07 19:54:39 +0100

    Replace the for loop in getDiceRoll() using a new Array constructor

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -15,11 +15,9 @@ const monster = {
 };
 
 function getDiceRollArray(diceCount) {
-  let newDiceRolls = [];
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return newDiceRolls;
+  return (newDiceRolls = new Array(diceCount)
+    .fill(0)
+    .map((num) => (num = Math.floor(Math.random() * 6) + 1)));
 }
 
 function getDiceHtml(diceCount) {
