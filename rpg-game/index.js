9f26703f15d5cbe805626bb25ccc2ec78844b6ed
Jake Samuels <samuels242@googlemail.com>
2022-07-08 16:32:12 +0100

    Export Character constructor to Character.js

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -1,38 +1,5 @@
 import characterData from "./data.js";
-import { getDiceRollArray } from "./utils.js";
-
-// function getDiceRollArray(diceCount) {
-//   return new Array(diceCount).fill(0).map(function () {
-//     return Math.floor(Math.random() * 6) + 1;
-//   });
-// }
-
-function Character(data) {
-  Object.assign(this, data);
-
-  this.getDiceHtml = function (diceCount) {
-    return getDiceRollArray(diceCount)
-      .map(function (num) {
-        return `<div class="dice">${num}</div>`;
-      })
-      .join("");
-  };
-
-  this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this;
-    let diceHtml = this.getDiceHtml(diceCount);
-
-    return `
-          <div class="character-card">
-              <h4 class="name"> ${name} </h4>
-              <img class="avatar" src="${avatar}" />
-              <div class="health">health: <b> ${health} </b></div>
-              <div class="dice-container">
-                  ${diceHtml}
-              </div>
-          </div>`;
-  };
-}
+import Character from "./Character.js";
 
 function render() {
   document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
