32c65bdab75c8801cec49b3895160ffa7e6d72ca
Jake Samuels <samuels242@googlemail.com>
2022-07-08 16:28:18 +0100

    Export getDiceRollArray() from utils.js

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -1,10 +1,11 @@
 import characterData from "./data.js";
+import { getDiceRollArray } from "./utils.js";
 
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6) + 1;
-  });
-}
+// function getDiceRollArray(diceCount) {
+//   return new Array(diceCount).fill(0).map(function () {
+//     return Math.floor(Math.random() * 6) + 1;
+//   });
+// }
 
 function Character(data) {
   Object.assign(this, data);
