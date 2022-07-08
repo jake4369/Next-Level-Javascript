dd3de8abab735e8e633f4aaf3eb5ee48a66b1e78
Jake Samuels <samuels242@googlemail.com>
2022-07-08 14:49:09 +0100

    Aside: Object.assign() challenge

--- aside/index.js
+++ aside/index.js
@@ -218,3 +218,28 @@ function Student(data) {
 const newStudent = new Student(studentDetails);
 newStudent.summariseStudent();
 */
+
+// Aside: Object.assign challenge
+const sandraKayeProfileData = {
+  name: "Sandra Kaye",
+  portfolio: "www.sandrasportfolio.com",
+  currentJob: "Google",
+  currentSalary: "400k",
+};
+
+/*
+Challenge
+1. Use Object.assign to cut 4 lines of code from the constructor function.
+*/
+
+function DevProfile(data) {
+  Object.assign(this, data);
+  this.summariseDev = function () {
+    console.log(
+      `${this.name}'s porfolio is at ${this.portfolio} and they work at ${this.currentJob}. Their current salary is ${this.currentSalary}.`
+    );
+  };
+}
+
+const sandraKaye = new DevProfile(sandraKayeProfileData);
+sandraKaye.summariseDev();
