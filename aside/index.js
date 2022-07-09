cf0dbf51d1d772d9f578cdfaa2aaf2cbb04aae6a
Jake Samuels <samuels242@googlemail.com>
2022-07-09 20:26:01 +0100

    Aside: inline arrow function challenge

--- aside/index.js
+++ aside/index.js
@@ -302,7 +302,7 @@ console.log(alertSpend(100));
 */
 
 // Aside: Arrow functions challenge
-
+/*
 // function speedWarning(speed){
 //   return `You are going at ${speed} mph!`
 // }
@@ -316,3 +316,17 @@ const speedWarning = (speedLimit, speed) => {
 };
 
 console.log(speedWarning(50, 40));
+*/
+
+// Aside: inline arrow function challenge
+const distanceTraveledMiles = [267, 345, 234, 190, 299];
+
+// const distanceTraveledKm = distanceTraveledMiles.map(function(distance){
+//     return Math.round(distance * 1.6)
+// })
+
+distanceTraveledKm = distanceTraveledMiles.map((distance) =>
+  Math.round(distance * 1.6)
+);
+
+console.log(distanceTraveledKm);
