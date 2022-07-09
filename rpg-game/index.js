2d4813223b813094b5138f491bdd3ebd596bcf33
Jake Samuels <samuels242@googlemail.com>
2022-07-09 19:34:31 +0100

    Render winner in endGame() function

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -18,7 +18,19 @@ function attack() {
 }
 
 function endGame() {
-  console.log("Game over");
+  const endMessage =
+    wizard.health === 0 && orc.health === 0
+      ? "No victors - all creatures are dead"
+      : wizard.health > 0
+      ? "The Wizard Wins"
+      : "The Orc is Victorious";
+
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
+  document.body.innerHTML = `<div class="end-game">
+      <h2>Game Over</h2>
+      <h3>${endMessage}</h3>
+      <p class="end-emoji">${endEmoji}</p>
+  </div>`;
 }
 
 document.getElementById("attack-button").addEventListener("click", attack);
