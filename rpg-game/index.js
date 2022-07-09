c55eff5a1b55a17e8e7478296243252645f6c8b2
Jake Samuels <samuels242@googlemail.com>
2022-07-09 23:05:14 +0100

    Render new monster whenever previous monster dies

--- rpg-game/index.js
+++ rpg-game/index.js
@@ -1,25 +1,34 @@
 import characterData from "./data.js";
 import Character from "./Character.js";
 
+let monstersArray = ["orc", "demon", "goblin"];
+
 function render() {
   document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+  document.getElementById("monster").innerHTML = monster.getCharacterHtml();
 }
 
 function attack() {
   wizard.getDiceHtml();
-  orc.getDiceHtml();
-  wizard.takeDamage(orc.currentDiceScore);
-  orc.takeDamage(wizard.currentDiceScore);
+  monster.getDiceHtml();
+  wizard.takeDamage(monster.currentDiceScore);
+  monster.takeDamage(wizard.currentDiceScore);
   render();
-  if (wizard.dead || orc.dead) {
+  if (wizard.dead) {
     endGame();
+  } else if (monster.dead) {
+    if (monstersArray) {
+      monster = getNewMonster();
+      render();
+    } else {
+      endGame();
+    }
   }
 }
 
 function endGame() {
   const endMessage =
-    wizard.health === 0 && orc.health === 0
+    wizard.health === 0 && monster.health === 0
       ? "No victors - all creatures are dead"
       : wizard.health > 0
       ? "The Wizard Wins"
@@ -33,9 +42,14 @@ function endGame() {
   </div>`;
 }
 
+function getNewMonster() {
+  const nextMonsterData = characterData[monstersArray.shift()];
+  return nextMonsterData ? new Character(nextMonsterData) : {};
+}
+
 document.getElementById("attack-button").addEventListener("click", attack);
 
 const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
+let monster = getNewMonster();
 
 render();
